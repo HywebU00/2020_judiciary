ffd561ce54347babf8ffaa4551c09e9e849ba48c
anniewu <annie@hyweb.com.tw>
2020-11-11 15:17:51 +0800

    20201111 修改table 間隔顏色

--- js/hyui.js
+++ js/hyui.js
@@ -892,7 +892,7 @@ $(function() {
     // /*------------------------------------*/
     $('table').each(function(index, el) {
         //判斷沒有table_list
-        if ($(this).parents('.table_list').length == 0 && $(this).parents('.fix_th_table').length == 0 && $(this).parent('form').length == 0 && $(this).parent('.table_sprite').length == 0) {
+        if ($(this).parents('.table_list').length == 0 && $(this).parents('.fix_th_table').length == 0 && $(this).parent('form').length == 0 && $(this).parent('.table_sprite').length == 0 && $(this).parent('.table_thleft').length == 0) {
             $(this).scroltable();
         }
     });
