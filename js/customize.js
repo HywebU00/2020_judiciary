c9eb216444f403c13b4519b18ffed2f9a9ad7ef8
anniewu <annie@hyweb.com.tw>
2020-11-16 15:41:50 +0800

    20201116 修改js

--- js/customize.js
+++ js/customize.js
@@ -547,7 +547,7 @@ $(function() {
     $('.qa_list>ul>li,.meeting_list>ul>li').each(function() {
         $(this).find('.answer').hide();
         var _qaItem = $(this).children('.question').children('a');
-        var _scrollTop = $(this).offset().top - 300;
+        var _scrollTop = $(this).offset().top - 55;
         _qaItem.each(function() {
             function qa(e) {
                 $('html, body').stop(true, true).animate({ scrollTop: _scrollTop }, 100, 'easeOutExpo');
